be4dec2c995ee094dc1fc0bfbafde96952451666
Kevin Werner <wernke@akros.ch>
2021-01-27 21:46:27 +0100

    #6 BugFix default number of courses

--- graph-testdata-generator/src/main/java/ch/swb/graphgenerator/graph/DefaultGraphParameters.java
+++ graph-testdata-generator/src/main/java/ch/swb/graphgenerator/graph/DefaultGraphParameters.java
@@ -9,7 +9,7 @@ public class DefaultGraphParameters {
 	public static final int DEFAULT_NUMBER_OF_CERTIFICATES = 60;
 	public static final int DEFAULT_NUMBER_OF_KNOWLEDGES = 66;
 	public static final int DEFAULT_NUMBER_OF_SKILLS = 60;
-	public static final int DEFAULT_NUMBER_OF_COURSES = 70;
+	public static final int DEFAULT_NUMBER_OF_COURSES = 66;
 	public static final int DEFAULT_NUMBER_OF_PROJECTS = 30;
 //	public static final int DEFAULT_NUMBER_OF_ROLES = 15;
 
